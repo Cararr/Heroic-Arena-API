3baac9cea79de7d1324a4511a0d305bb6bda231b
Car <cararr@tlen.pl>
2021-02-28 17:15:28 +0100

    added get all heroes route

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express'),
 	cors = require('cors'),
 	errorHandler = require('errorhandler'),
 	worldsRouter = require('./Routers/worldsRoutes/worldsRouter');
-const server = express();
+const server = express(),
+	sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./database.sqlite');
+
 const PORT = process.env.PORT || 4000;
 
 server.use(express.json());
@@ -11,6 +14,12 @@ server.use(morgan('dev'));
 server.use(cors());
 
 server.use('/worlds', worldsRouter);
+
+server.get('/heroes', (req, res, next) => {
+	db.all(`SELECT * FROM Hero;`, (error, heroes) => {
+		error ? next(error) : res.send({ heroes });
+	});
+});
 
 server.use(errorHandler());
 server.listen(PORT, () => console.log(`CArArr on ${PORT}`));
